5101eb6b844059958155218d16eff7b68b2e87d7
dominic.codespoti <dominic.codespoti@myob.com>
2021-06-26 02:45:18 +1000

    Add empty test suite

---
+++ Tests/KoganCodingChallengeTests/Core/CubicWeightServiceTests.cs
@@ -0,0 +1,17 @@
+namespace KoganCodingChallengeTests.Core
+{
+    using Xunit;
+
+    public class CubicWeightServiceTests
+    {
+        [Fact]
+        public void GivenListOfProducts_WhenCategoryIsAirConditioner_ShouldCalculateCubicWeightOfOnlyAirConditionerProducts()
+        {
+        }
+        
+        [Fact]
+        public void GivenListOfProducts_ShouldCalculateCubicWeightCorrectly()
+        {
+        }
+    }
+}
